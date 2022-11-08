8f4c2e8c5d35d5aff41c235865010205a488f7e0
Ather1230 <ather.biswas0057@gmail.com>
2022-11-08 15:44:21 +0600

    features added

---
+++ src/pages/Home/Features/Features.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaUserFriends, FaStar, FaStackExchange } from 'react-icons/fa';
+import { BsClock } from "react-icons/bs";
+
+
+const Features = () => {
+    return (
+        <div className="grid grid-cols-4 border border-indigo-400 gap-4 mt-10 p-5">
+            <div className='text-white mx-auto'>
+                <FaUserFriends className='text-7xl mx-4'></FaUserFriends>
+                <div className='text-xl font-semibold text-center'>
+                <h4 >1k+</h4>
+                <h4>Customers</h4>
+                </div>
+            </div>
+            <div className='text-white mx-auto'>
+                <FaStackExchange className='text-7xl mx-4'></FaStackExchange>
+                <div className='text-xl font-semibold text-center'>
+                <h4 >10+</h4>
+                <h4>Services</h4>
+                </div>
+            </div>
+            <div className='text-white mx-auto'>
+                <BsClock className='text-7xl mx-4'></BsClock>
+                <div className='text-xl font-semibold text-center'>
+                <h4 >5-10 min</h4>
+                <h4>Completed</h4>
+                </div>
+            </div>
+            <div className='text-white mx-auto'>
+                <FaStar className='text-7xl mx-4'></FaStar>
+                <div className='text-xl font-semibold text-center'>
+                <h4 >1.5k+</h4>
+                <h4>Review</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Footer from "../../shared/Footer/Footer";
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Features from "../Features/Features";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <h1 className="text-white text-center mt-7 text-4xl font-bold">About Me</h1>
       <About></About>
+      <h1 className="text-white text-center mt-7 text-4xl font-bold">Features</h1>
+      <Features></Features>
       <Footer></Footer>
     </div>
   );
